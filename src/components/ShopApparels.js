c7242aaf83f81d242413b0758a6dfc1aeb27d13a
TamakiSaito <immacd1012@gmail.com>
2024-04-06 20:49:18 +0900

    feat:shopApparel1を作成

--- src/components/ShopApparels.js
+++ src/components/ShopApparels.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import "./ShopApparels.css";
 
 export default function ShopApparels() {
+  const [apparels, setApparels] = useState([]);
+
+  // mounted
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        const items = json.filter((item) => item.category === "men's clothing");
+        setApparels(items);
+      });
+  }, []);
+
+  const renderApparels = () => {
+    return apparels.map((apparel) => (
+      <div key={apparel.id} className="apparelContainer">
+        <div className="apparelTile">{apparel.title}</div>
+        <div className="apparelPrice">${apparel.price}</div>
+        {apparel.image && (
+          <img
+            src={apparel.image}
+            alt={apparel.title}
+            className="apparelImage"
+          />
+        )}
+        <div className="apparelDescription">{apparel.description}</div>
+      </div>
+    ));
+  };
+
   return (
-    <div>
+    <div className="shopApparels">
       <h1>ShopApparels</h1>
-      <div>Contents</div>
+      <div className="apparelsContainer">{renderApparels()}</div>
     </div>
   );
 }
