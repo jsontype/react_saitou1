aff1733816a8bd43554f3f7e371d9faf3751062d
yuntic6ix <yunsuku6@gmail.com>
2024-04-07 00:27:45 +0900

    feat:shopjewelreisアップを追加

--- src/components/ShopJewelries.js
+++ src/components/ShopJewelries.js
@@ -5,24 +5,42 @@ import "./ShopJewelries.css";
 export default function ShopJewelries() {
   const [jewelries, setJewelries] = useState([]);
 
+  const jewelRatingClass = (rating) => {
+    return rating.rate >= 4 ? "good" : rating.rate >= 3 ? "soso" : "bad"
+  }
+
   // mounted
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((json) => {
-        const items = json.filter((item) => item.category === "jewelery");
-        setJewelries(items);
-      });
-  }, []);
+        const items = json.filter((item) => item.category === "jewelery")
+        setJewelries(items)
+      })
+  }, [])
+
+  console.log("jewelries:", jewelries)
 
   const render = jewelries.map((jewel) => {
     return (
       <div key={jewel.id}>
-        <div>{jewel.title}</div>
-        <div>${jewel.price}</div>
+        <div className="jewelTitle">
+          #{jewel.id} {jewel.title}
+        </div>
+        <div className="jewelPrice">価格：${jewel.price}</div>
+        <div>
+          評価：
+          <span className={jewelRatingClass(jewel.rating)}>
+            {jewel.rating.rate}
+          </span>
+          /5点
+        </div>
+        <div className="jewelCount">評価した人数：{jewel.rating.count}人</div>
+        <p>製品説明：{jewel.description}</p>
+        <img className="jewelImg" src={jewel.image} alt={jewel.title}></img>
       </div>
-    );
-  });
+    )
+  })
 
   return (
     <div>
